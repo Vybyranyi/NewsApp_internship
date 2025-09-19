b71bb2207a1627114f9f9ab2992ab1763587ba5d
Vybyranyi <mariankvit@gmail.com>
2025-09-19 14:03:20 +0300

    Refactor components to use React Query for data fetching and add authentication store

--- src/pages/FullNewsPage.tsx
+++ src/pages/FullNewsPage.tsx
@@ -1,41 +1,54 @@
+import { useQuery } from "@tanstack/react-query";
 import { Card, message, Typography } from "antd";
-import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import newsData from "../mock/news.json";
 
 const { Title, Paragraph } = Typography;
 
-interface NewsItem {
-	id: number;
-	title: string;
-	description: string;
-	content: string;
-}
+const fetchNewsById = async (id: number) => {
+	const item = newsData.find((news) => news.id === id);
+	if (!item) {
+		throw new Error("Новину не знайдено!");
+	}
+	return item;
+};
 
 export default function FullNewsPage() {
 	const { id } = useParams<{ id: string }>();
-	const [newsItem, setNewsItem] = useState<NewsItem | null>(null);
 
-	useEffect(() => {
-		const foundItem = newsData.find((item) => item.id === Number(id));
+	const {
+		data: newsItem,
+		isLoading,
+		isError,
+		error,
+	} = useQuery({
+		queryKey: ["news", id],
+		queryFn: () => fetchNewsById(Number(id)),
+	});
 
-		if (foundItem) {
-			setNewsItem(foundItem);
-		} else {
-			message.error("Новину не знайдено!");
-		}
-	}, [id]);
+	if (isLoading) {
+		return <div className="text-center mt-8">Завантаження...</div>;
+	}
 
-	if (!newsItem) {
-		return <div>Завантаження...</div>;
+	if (isError) {
+		message.error(error.message);
+		return (
+			<div className="text-center mt-8 text-red-500">
+				Помилка: {error.message}
+			</div>
+		);
 	}
 
 	return (
 		<div className="mx-auto p-4 max-w-4xl">
 			<Card className="w-full shadow-md">
-				<Title level={2}>{newsItem.title}</Title>
-				<Paragraph className="mb-4">{newsItem.description}</Paragraph>
-				<Paragraph>{newsItem.content}</Paragraph>
+				{newsItem && (
+					<>
+						<Title level={2}>{newsItem.title}</Title>
+						<Paragraph className="mb-4">{newsItem.description}</Paragraph>
+						<Paragraph>{newsItem.content}</Paragraph>
+					</>
+				)}
 			</Card>
 		</div>
 	);
